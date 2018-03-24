980e0b1dfc8d8924af27acdb7c82576e93a8388e
Dave Chan <davechan813@gmail.com>
2018-03-24 12:28:44 -0700

    upgradep the profile page. Author: Zhibang Chen

--- Jokus/Controller/ProfileViewController.swift
+++ Jokus/Controller/ProfileViewController.swift
@@ -14,6 +14,8 @@ class ProfileViewController: UIViewController {
     
     @IBOutlet weak var MsgContainer: UIView!
     @IBOutlet weak var FeedContainer: UIView!
+    @IBOutlet weak var InvitationButton: UIButton!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -27,6 +29,12 @@ class ProfileViewController: UIViewController {
         FeedContainer.layer.cornerRadius = 25;
         FeedContainer.layer.borderWidth = 0.5
         FeedContainer.layer.borderColor = UIColor.clear.cgColor
+        
+        
+        InvitationButton.layer.cornerRadius = 35;
+        InvitationButton.layer.borderWidth = 0.5
+        InvitationButton.layer.borderColor = UIColor.clear.cgColor
+        
     }
 
     override func didReceiveMemoryWarning() {
